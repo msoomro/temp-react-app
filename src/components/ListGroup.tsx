a6374c5f3024620472be9833ace3bb0c2112a056
msoomro <moomalmsoomro@gmail.com>
2026-01-29 00:47:46 -0500

    Highlight selected item in list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 
 function ListGroup() {
     let items = ["list item 1", "list item 2"];
-    items = [];
     
-    
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
     return (
         <>
             <h1>List Group</h1>
-            {items.length === 0 ? <p>No item found</p> : null}
+            {/* use and logic instead of ternary operator for conciseness*/}
+            {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
-                {items.map(item => (
+                {items.map((item, index) => (
                     // to overcome warning that each child in a list needs a "unique key", add key tag. for now can use the item itself since each item ought to be distinct else collapse to one item still works functionally
-                    <li key={item} className="list-group-item">{item}</li>
+                    <li 
+                        className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+                        key={item} 
+                        onClick={() => setSelectedIndex(index)} 
+                    >
+                        {item}
+                    </li>
                 ))}
             </ul>
         </>
