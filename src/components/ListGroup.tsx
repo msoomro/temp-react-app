a2a28ca3a2d5cbd0d746321d156ec10ac2fe1087
msoomro <moomalmsoomro@gmail.com>
2026-01-28 23:59:47 -0500

    Make list dynamic

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,20 @@
 
 function ListGroup() {
+    let items = ["list item 1", "list item 2"];
+    items = [];
+    
+    
     return (
         <>
-        <h1>List Group</h1>
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+            <h1>List Group</h1>
+            {items.length === 0 ? <p>No item found</p> : null}
+            <ul className="list-group">
+                {items.map(item => (
+                    // to overcome warning that each child in a list needs a "unique key", add key tag. for now can use the item itself since each item ought to be distinct else collapse to one item still works functionally
+                    <li key={item} className="list-group-item">{item}</li>
+                ))}
+            </ul>
         </>
-        
     );
 }
 
